d9be30f38342ee51181f6223971aa56611215b32
Diego Leonardo <diegoleonardo9966@gmail.com>
2026-04-14 12:27:45 -0300

    projeto inicial

---
+++ index.js
@@ -0,0 +1,9 @@
+function apagar(){
+    let el = document.getElementById("entrada")
+    el.innerText = ''
+}
+
+function somarTexto(num){
+    let el = document.getElementById("entrada")
+    el.innerText += num
+}
